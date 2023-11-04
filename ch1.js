269711cb6c747c87b980703cadfc582b224a577d
Santiago Suarez <santiagosuarez@fymtech.com>
2023-11-03 23:55:21 -0500

    Challenge_1

---
+++ ch1.js
@@ -0,0 +1,35 @@
+async function getData() {
+  const res = await fetch("https://codember.dev/data/message_01.txt");
+  const data = await res.text();
+  return data.replace("\n", "").split(" ");
+}
+
+function countWords(data) {
+  return data.reduce((acc, cur) => {
+    const current = cur.toLowerCase();
+    const array = Array.isArray(acc) ? [...acc] : [{ name: acc, count: 1 }];
+    const index = array.findIndex((i) => i.name === current);
+
+    if (index > -1) {
+      array[index].count++;
+      return array;
+    }
+
+    array.push({ name: current, count: 1 });
+    return array;
+  });
+}
+
+function printWordsCounts(wordsArray) {
+  let result = "";
+  for (const { name, count } of wordsArray) {
+    result += `${name}${count}`;
+  }
+  console.log(result);
+}
+
+(async () => {
+  const data = await getData();
+  const wordsArray = countWords(data);
+  printWordsCounts(wordsArray);
+})();
